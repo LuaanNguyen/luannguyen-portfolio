a4faca3322afa5296f52ad1c58528f45db27a84f
LuaanNguyen <luanthiennguyen2003@gmail.com>
2024-07-19 16:21:21 -0700

    Setting Up the Right Tailwind Configs 💅

--- src/app/components/Navigation.tsx
+++ src/app/components/Navigation.tsx
@@ -5,12 +5,12 @@ import "../globals.css";
 export default function Navigation() {
   return (
     <nav className="w-screen">
-      <div className=" flex justify-between mx-20 mt-10 border border-white p-5 rounded-lg items-center">
-        <div className="flex  gap-5 text-3xl font-bold">
+      <div className="flex justify-between mx-20 mt-10 border border-white p-5 rounded-full items-center">
+        <div className="flex gap-5 text-3xl font-bold">
           <FaCode />
-          <h1 className="text-spotify-green">Luan Nguyen</h1>
+          <h1 className="">Luan Nguyen</h1>
         </div>
-        <div>Social Links</div>
+        <div className="">Social Links</div>
       </div>
     </nav>
   );
