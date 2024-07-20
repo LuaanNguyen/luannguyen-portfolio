29125b765758ae9bbf9fd911ad3b39db3700c12f
LuaanNguyen <luanthiennguyen2003@gmail.com>
2024-07-19 17:41:45 -0700

    Added Profile Image to Mobile Navigation

--- src/app/components/Navigation.tsx
+++ src/app/components/Navigation.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import React, { useState } from "react";
+import Image from "next/image";
 import { FaCode } from "react-icons/fa6";
 import { ReactNode } from "react";
 import { FaInstagram } from "react-icons/fa6";
@@ -14,40 +15,60 @@ import { FaTimes } from "react-icons/fa";
 import Link from "next/link";
 
 export default function Navigation(): ReactNode {
-  const [mobileNav, setMobileNav] = useState<boolean>(false);
+  const [mobile, setMobile] = useState<boolean>(false);
 
-  function handleMobileNav() {
-    setMobileNav((prev) => !prev);
+  function handleMobile() {
+    setMobile((prev) => !prev);
   }
 
   return (
     <nav className="w-screen">
-      <div className="flex bg-spotify-light-dark justify-between max-md:mx-2 max-md:mt-4  max-md:px-5 max-md:py-3 mx-32 mt-10 p-2 rounded-full items-center">
-        {/*Mobile Mode*/}
+      <div
+        className={`flex flex-col bg-spotify-light-dark max-md:mx-2 max-md:mt-4 mx-32 mt-10 p-2 rounded-full
+         items-center`}
+      >
+        <section className="flex flex-row w-full justify-between items-center max-md:px-4 max-md:py-1">
+          {/*Mobile Mode*/}
+          {mobile ? (
+            <FaTimes className="text-4xl sm:hidden" onClick={handleMobile} />
+          ) : (
+            <GiHamburgerMenu
+              className="text-4xl sm:hidden"
+              onClick={handleMobile}
+            />
+          )}
 
-        {mobileNav ? (
-          <FaTimes className="text-4xl sm:hidden" onClick={handleMobileNav} />
-        ) : (
-          <GiHamburgerMenu
-            className="text-4xl sm:hidden"
-            onClick={handleMobileNav}
-          />
-        )}
+          <Link href="/" className="flex gap-2 text-3xl font-bold items-center">
+            {mobile ? (
+              <Image
+                src="/Luan.jpg"
+                alt="Luan Nguyen"
+                width={40}
+                height={40}
+                className="rounded-full aspect-square object-cover"
+              />
+            ) : (
+              <FaCode className="text-spotify-green w-[70px]" />
+            )}
 
-        <Link href="/" className="flex gap-2 text-3xl font-bold items-center">
-          <FaCode className="text-spotify-green w-[70px]" />
-          <h1 className="max-lg:hidden">Luan Nguyen</h1>
-        </Link>
+            <h1 className="max-lg:hidden">Luan Nguyen</h1>
+          </Link>
 
-        {/*Hidden on Mobile*/}
-        <div className="flex flex-row gap-5 items-center max-md:hidden">
-          <SocialLinks />
-          <PiLineVerticalThin className="text-spotify-gray text-4xl" />
-          <Button name="Let's Talk" />
-        </div>
+          {/*Hidden on Mobile*/}
+          <section className="flex flex-row gap-5 items-center max-sm:hidden">
+            <SocialLinks />
+            <PiLineVerticalThin className="text-spotify-gray text-4xl" />
+            <Button name="Let's Talk" />
+          </section>
+        </section>
+        {mobile && <MobileNav />}
       </div>
     </nav>
   );
+}
+
+function MobileNav() {
+  return <nav className="bg-spotify-light-dark justify-left"></nav>;
 }
 
 interface ButtonProps {
