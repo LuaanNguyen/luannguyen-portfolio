29cc000c5e910d5782bb644f3205dff1ab9f1150
LuaanNguyen <luanthiennguyen2003@gmail.com>
2024-07-20 11:20:08 -0700

    Fixed `react/no-unescaped-entities`

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -4,7 +4,7 @@ export default function Footer() {
   return (
     <div className="col-span-6 items-center text-center">
       <p className="text-spotify-light-gray">
-        @ Luan's Portfolio {new Date().getFullYear()}, Design By{" "}
+        &copy; Luan's Portfolio {new Date().getFullYear()}, Design By{" "}
         <a
           href="https://www.luannguyen.net/"
           className="text-spotify-green font-semibold underline"
