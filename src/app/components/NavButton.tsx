ad5d704855b089a8a29a68de8e687c24af0d1568
LuaanNguyen <luanthiennguyen2003@gmail.com>
2024-08-26 12:25:50 -0700

    Updated Resume 💅

--- src/app/components/NavButton.tsx
+++ src/app/components/NavButton.tsx
@@ -7,10 +7,9 @@ interface ButtonProps {
 export default function NavButton({ name }: ButtonProps) {
   return (
     <a
-      href="https://drive.google.com/file/d/1s1neHJHyNp7HS9GUeh28QABNzOmkzPJh/view?usp=sharing"
+      href="https://drive.google.com/file/d/1C6jRwqU4CpUszvz4KuWLgzdxI3iXqkRP/view?usp=sharing"
       target="_blank"
     >
-      {" "}
       <button
         type="button"
         className="flex items-center text-md px-4 py-3 rounded-full bg-spotify-green  font-semibold hover:bg-spotify-dark-green gap-2"
